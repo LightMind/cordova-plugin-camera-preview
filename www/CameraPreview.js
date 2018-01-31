90dad1defecf63fb06b663b048224ed337f7cff3
Lukas Walther <lukas@creativevikings.com>
2018-01-31 10:39:51 +0100

    wat.

--- www/CameraPreview.js
+++ www/CameraPreview.js
@@ -58,6 +58,45 @@ CameraPreview.show = function(onSuccess, onError) {
     exec(onSuccess, onError, PLUGIN_NAME, "showCamera", []);
 };
 
+CameraPreview.takePictureToFile = function(opts, onSuccess, onError) {
+    if (!opts) {
+        opts = {};
+    } else if (isFunction(opts)) {
+        onSuccess = opts;
+        opts = {};
+    }
+
+    if (!isFunction(onSuccess)) {
+        return false;
+    }
+
+    opts.width = opts.width || 0;
+    opts.height = opts.height || 0;
+
+    if (!opts.quality || opts.quality > 100 || opts.quality < 0) {
+        opts.quality = 85;
+    }
+
+    var fileName = opts.fileName || "tempFile.jpg";
+
+    if(typeof opts.orientation == "undefined" || opts.orientation == null){
+        opts.orientation = 0;
+    }
+
+    exec(
+        onSuccess,
+        onError,
+        PLUGIN_NAME,
+        "takePictureToFile",
+        [
+            opts.width,
+            opts.height,
+            opts.quality,
+            fileName,
+            opts.orientation,
+        ]);
+};
+
 CameraPreview.takePicture = function(opts, onSuccess, onError) {
     if (!opts) {
         opts = {};
@@ -75,6 +114,10 @@ CameraPreview.takePicture = function(opts, onSuccess, onError) {
 
     if (!opts.quality || opts.quality > 100 || opts.quality < 0) {
         opts.quality = 85;
+    }
+
+    if(typeof opts.orientation == "undefined" || opts.orientation == null){
+        opts.orientation = 0;
     }
 
     exec(onSuccess, onError, PLUGIN_NAME, "takePicture", [opts.width, opts.height, opts.quality]);
